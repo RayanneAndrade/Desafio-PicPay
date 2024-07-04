96394fb9eb29ee0af41aa1458447bf7c77c14df2
rayanneandrade <rayanneandrade266@hotmail.com>
2024-07-04 20:32:32 -0300

    Revert "Revert "Adicionando projeto desafio Pic Pay""
    This reverts commit c23c5ee35e87d2c02bf85bd13d3fa3baa9d0785a.

---
+++ src/main/java/tech/buildrun/picpay/exception/WalletDataAlreadyExistsException.java
@@ -0,0 +1,23 @@
+package tech.buildrun.picpay.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+
+public class WalletDataAlreadyExistsException extends PicPayException {
+
+    public String detail;
+
+    public WalletDataAlreadyExistsException(String detail) {
+        this.detail = detail;
+    }
+
+    public ProblemDetail toProblemDetail() {
+        var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        pb.setTitle("Wallet data already exists");
+        pb.setDetail(detail);
+
+        return pb;
+    }
+}
